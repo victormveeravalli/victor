fe24715259b4557bd49a41fc8fa4d0bd98388854
Sony Angel <sonyangel.kakileti@dreamstep.in>
2020-06-26 13:09:19 +0530

    no message

--- order-management-service/src/main/java/com/manage/order/repository/OrderDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.manage.order.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.manage.order.dto.OrderDetails;
-
-public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Long> { 
-
-}
